f2b3a3e85f9743cf44c64e1f862daa1afbb4272c
PoQuatre <leflem.mateo2002@gmail.com>
2022-04-13 13:58:15 +0200

    Add backend

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+const port = 8080;
+
+const students = ["Student 1", "Student 2"];
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/students", (req, res) => {
+  res.json(students);
+});
+
+app.post("/students", (req, res) => {
+  const name = req.body.name;
+
+  if (name && typeof name === "string" && name.trim().length > 0) {
+    students.push(name.trim());
+
+    res.send("ok");
+  } else {
+    res.status(400).send("Invalid name");
+  }
+});
+
+app.listen(port, () => {
+  console.log("The server started listening on port", port);
+});
